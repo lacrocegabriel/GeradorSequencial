b5b8ce043ce0ab491949d5cd58b7f6de45d1ca6c
Gabriel Lacroce <gp.lacroce@gmail.com>
2022-06-13 22:16:31 -0300

    Criação da lógica para não gerar números iguais

--- GeradorSequencial/Program.cs
+++ GeradorSequencial/Program.cs
@@ -1,12 +1,38 @@
 ﻿var random = new Random();
+var list = new List<int>();
+int NumeroGerado = 0;
 int quantidade;
+int j = 0;
+
 
 Console.WriteLine("Digite a sequencia de números que irá utilizar");
 quantidade = int.Parse(Console.ReadLine());
 
 for (int i = 0; i < quantidade; i++)
 {
-    Console.WriteLine(random.Next(1, 60));
+    j = 0;
+    NumeroGerado = random.Next(1, 60);
+    
+    while (j == 0)
+    {
+        if (list.Contains(NumeroGerado))
+        {
+            NumeroGerado = random.Next(1, 60);
+            j = 0;
+        }
+        else
+        {
+            j = 1;
+        }
+    }
+    list.Add(NumeroGerado);
+
+}
+
+
+foreach (int n in list)
+{
+    Console.Write(n.ToString()," - ");
 }
 
 
