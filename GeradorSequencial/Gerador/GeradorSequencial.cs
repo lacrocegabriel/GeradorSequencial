14a0f00c00fede7f5de6dd94097632109e43a846
Gabriel Lacroce <gp.lacroce@gmail.com>
2022-06-13 21:48:31 -0300

    Add project files.

---
+++ GeradorSequencial/Gerador/GeradorSequencial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorSequencial.Gerador
+{
+    public class GeradorSequencial
+    {
+        public int Numero { get; set; }
+
+        public GeradorSequencial(int numero)
+        {
+            Numero = numero;
+        }
+
+        
+    }
+}
---
+++ GeradorSequencial/Program.cs
@@ -0,0 +1,16 @@
+﻿var random = new Random();
+int quantidade;
+
+Console.WriteLine("Digite a sequencia de números que irá utilizar");
+quantidade = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < quantidade; i++)
+{
+    Console.WriteLine(random.Next(1, 60));
+}
+
+
+
+
+
+
